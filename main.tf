16351d2fa8f545ee46870e734aa0c6519300fb8b
srikanth_j <srikanth_j@apple.com>
2025-05-05 18:01:09 +0530

    adding main-tf for s3 bucket

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.97.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_id" "rand_id" {
+  byte_length = 6
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "tf-cloud-${terraform.workspace}-${random_id.rand_id.hex}"
+}
